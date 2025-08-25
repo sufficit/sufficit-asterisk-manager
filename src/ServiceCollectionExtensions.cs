20c4e2a0d4dc2e78c9fa066b02293f56f8f519d4
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-08-25 16:15:25 -0300

    Refatora ServiceCollectionExtensions e atualiza opções de versionamento no projeto

--- src/ServiceCollectionExtensions.cs
+++ src/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using AsterNET;
+using AsterNET;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,13 +10,18 @@ namespace Sufficit.Asterisk.Manager
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddSufficitAsteriskManager(this IServiceCollection services)
+        {
+            var provider = services.BuildServiceProvider(false);
+            var factory = provider.GetService<Microsoft.Extensions.Logging.ILoggerFactory>();
+            return services.AddSufficitAsteriskManager(factory);
+        }
+
         /// <summary>
         ///     For now, only sets a log factory for the Asterisk Manager Classes.
         /// </summary>
-        public static IServiceCollection AddSufficitAsteriskManager(this IServiceCollection services)
+        public static IServiceCollection AddSufficitAsteriskManager(this IServiceCollection services, ILoggerFactory? factory = null)
         {
-            var provider = services.BuildServiceProvider();
-            var factory = provider.GetService<Microsoft.Extensions.Logging.ILoggerFactory>();
             if (factory is not null)
             {
                 ManagerLogger.LoggerFactory = factory;
